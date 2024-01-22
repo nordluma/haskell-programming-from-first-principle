b2974bdb9979c0e9994088edd943c2284a820dd5
nordluma <maxnordlund.95@hotmail.com>
2024-01-22 22:40:20 +0200

    feat(ch2): exercises

--- ch_2/exercises.hs
+++ ch_2/exercises.hs
@@ -1,3 +1,4 @@
+-- Exercises 1
 -- 1.
 -- a
 half x = x / 2
@@ -7,3 +8,17 @@ square x = x * x
 
 -- 2.
 timesPi x = 3.14 * (x * x)
+
+-- Exercises 2
+-- 1.
+area x = 3.14 * (x * x)
+
+-- 2
+double x = x * 2
+
+-- 3
+x = 7
+
+y = 10
+
+f = x + y
