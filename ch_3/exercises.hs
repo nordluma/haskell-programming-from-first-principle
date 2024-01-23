2441d31e55c9b5033b59d9524d2080bde16eeca9
nordluma <maxnordlund.95@hotmail.com>
2024-01-23 20:36:37 +0200

    feat(ch3): intermission exercises

---
+++ ch_3/exercises.hs
@@ -0,0 +1,15 @@
+-- Exercises 1
+-- 4.
+area d = pi * (r * r)
+  where
+    r = d / 2
+
+-- Exercises 1
+-- 1.
+-- (++) [1, 2, 3] [4, 5, 6] || [1, 2, 3] ++ [4, 5, 6]
+
+-- 2.
+-- "<3" ++ " Haskell"
+
+-- 3.
+-- concat ["<3", " Haskell"]
