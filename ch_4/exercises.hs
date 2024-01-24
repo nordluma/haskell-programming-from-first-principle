e549c41b6294effbc6e399c707ffd77d005b8a80
nordluma <maxnordlund.95@hotmail.com>
2024-01-24 19:16:12 +0200

    feat(ch4): intermission exercises

--- ch_4/exercises.hs
+++ ch_4/exercises.hs
@@ -1,4 +1,4 @@
--- Intermission Exercises
+-- 1. Intermission Exercises
 -- 1.
 -- Mood is the type constructor and is also the name
 
@@ -11,3 +11,19 @@ data Mood = Blah | Woot deriving (Show)
 changeMood :: Mood -> Mood
 changeMood Blah = Woot
 changeMood Woot = Blah
+
+-- 2. Intermission Exercises
+-- 1.
+-- not True && True
+
+-- 2.
+-- not (x == 6)
+
+-- 3.
+-- (1 * 2) > 5
+
+-- 4.
+-- ["Merry"] > ["Happy"]
+
+-- 5.
+-- "1, 2, 3 " ++ "look at me!"
