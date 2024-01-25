1909709c1cec0a55e098a90ac0e994f232d26f8d
nordluma <maxnordlund.95@hotmail.com>
2024-01-25 22:46:21 +0200

    feat(ch5): intermission exercises

---
+++ ch_5/exercises.hs
@@ -0,0 +1,15 @@
+-- 1. Intermisson Exercises
+-- 1.
+-- not :: Bool -> Bool
+
+-- 2.
+-- length :: [a] -> Int / Foldable t => t a -> Int
+
+-- 3.
+-- concat :: Foldable t => [a] -> [a]
+
+-- 4.
+-- head :: [a] -> a
+
+-- 5.
+-- (<) :: Ord a => a -> a -> Bool
